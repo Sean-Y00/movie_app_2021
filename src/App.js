b4a6bc013d8f0f2c31781c92dbeec4d20dfb444a
Sean-Y00 <59878097+Sean-Y00@users.noreply.github.com>
2021-06-04 00:02:18 -0400

    state done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from "react";
 
-
-
 class App extends React.Component{
-// dont have return
-// have "render()"
-state = {
-  count: 0
-}
 
-
-add = () => {
-  let add = 0;
-  add = this.state.count+1;
-  return console.log(add);
-}
-minus = () => {
-  console.log("Minus");
-}
-
-
-render(){
-  return (
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-  );
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({ isLoading:false});
+    }, 6000);
   }
 
+  render(){
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+
+  }
 
 }
+
+
 export default App;
